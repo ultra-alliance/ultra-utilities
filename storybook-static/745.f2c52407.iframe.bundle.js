788ca03fb0ab897a275f35815ef5c54997bd9565
Oscar Macieira <macieiraoscar@icloud.com>
2023-08-26 11:18:17 +0200

    feat: generated folder for typedoc & storybook

--- storybook-static/745.f2c52407.iframe.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_ultra_alliance_ultra_utilities=self.webpackChunk_ultra_alliance_ultra_utilities||[]).push([[745],{"./node_modules/react-dom/client.js":(__unused_webpack_module,exports,__webpack_require__)=>{var m=__webpack_require__("./node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
